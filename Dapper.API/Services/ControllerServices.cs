6b9d98a0146761d8f9ec330056d4b4c4497951be
Kevin Weir <kevin.weir@outlook.com>
2021-02-13 20:20:51 -0600

    Applied a global filter indcating all the web api controllers return only the JSON format.

--- Dapper.API/Services/ControllerServices.cs
+++ Dapper.API/Services/ControllerServices.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using FluentValidation.AspNetCore;
 using Dapper.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dapper.API.Services
 {
@@ -9,12 +10,16 @@ namespace Dapper.API.Services
         public static void AddControllerServices(this IServiceCollection services)
         {
             // TODO Find a better way for FleuntValidation to find assembly with validation rules
-            services.AddControllers()
-               .AddFluentValidation(fv => {
-                   fv.RegisterValidatorsFromAssemblyContaining<CustomerPostDTO>();
-                   fv.RunDefaultMvcValidationAfterFluentValidationExecutes = true;
-                   fv.ImplicitlyValidateChildProperties = false;
-               });
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(new ProducesAttribute("application/json"));
+            })
+
+            .AddFluentValidation(fv => {
+                fv.RegisterValidatorsFromAssemblyContaining<CustomerPostDTO>();
+                fv.RunDefaultMvcValidationAfterFluentValidationExecutes = true;
+                fv.ImplicitlyValidateChildProperties = false;
+            });
         }
     }
 }
